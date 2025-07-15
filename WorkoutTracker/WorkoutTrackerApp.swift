3d15f522b59d8d0bc314709cf9547be499dd0ab4
404kevnotfound <mewzok7@yahoo.com>
2025-07-15 09:32:58 -0600

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Student on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
